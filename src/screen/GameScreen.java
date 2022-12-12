563f3693c43fec4c63e111c45ec5a5b91fd8f05b
Katawaytee <ktriampo@gmail.com>
2022-12-12 15:13:06 +0700

    Create class GameScreen add some methods

---
+++ src/screen/GameScreen.java
@@ -0,0 +1,23 @@
+package screen;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+public class GameScreen extends StackPane {
+
+	private final String backgroundURL;
+	
+	public GameScreen() {
+		super();
+		backgroundURL = ClassLoader.getSystemResource("").toString();
+	}
+	
+	private void createBackground() {
+		ImageView background = new ImageView(new Image(backgroundURL));
+		background.setPreserveRatio(true);
+		background.setFitWidth(1080);
+		getChildren().add(background);
+	}
+	
+}
