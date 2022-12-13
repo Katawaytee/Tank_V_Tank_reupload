c3f334ed862bac5fcdbc988bcb16f7efeb346297
Katawaytee <ktriampo@gmail.com>
2022-12-13 14:07:25 +0700

    Change MyBaseButton

--- src/button/MyBaseButton.java
+++ src/button/MyBaseButton.java
@@ -2,18 +2,30 @@ package button;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 
-public abstract class MyBaseButton extends Canvas {
+public abstract class MyBaseButton extends StackPane {
 
 	protected String pictureURL;
+	protected Circle circle;
 	
 	public MyBaseButton() {
-		super(50,50);
+		super();
+		setWidth(50);
+		setHeight(50);
 	}
 	
 	protected void addPicture() {
+		Canvas canvas = new Canvas(50,50);
 		Image image = new Image(pictureURL);
-		getGraphicsContext2D().drawImage(image,0,0,50,50);
+		canvas.getGraphicsContext2D().drawImage(image,0,0,50,50);
+		getChildren().add(canvas);
+	}
+	
+	protected void addCircle() {
+		circle = new Circle(25);
+		getChildren().add(circle);
 	}
 	
 	protected abstract void addListener();
